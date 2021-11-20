5b37e3b95637e7c1915e680ef149a2bdffedff2d
bemfes <91624232+bemfes@users.noreply.github.com>
2021-11-20 18:01:07 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+nums = list(map(int, input('Введите числа: ').split()))
+k = int(input('Введите номер элемента: '))
+def kbig(nums,k):
+    for i in range(len(nums)):
+        lowest_value_index = i
+        for z in range(i + 1, len(nums)):
+            if nums[z] < nums[lowest_value_index]:
+                lowest_value_index = z
+        nums[i], nums[lowest_value_index] = nums[lowest_value_index], nums[i]
+
+    print(nums[-k])
+kbig(nums,k)
